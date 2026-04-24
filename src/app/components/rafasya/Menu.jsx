38d3c3d1df8f29aa350f2150e75acadf6f137d53
RafasyaSubhan <rafasya.subhan@gmail.com>
2026-04-24 18:26:20 +0700

    Initial commit

---
+++ src/app/components/rafasya/Menu.jsx
@@ -0,0 +1,7 @@
+export default function Menu() {
+  return (
+    <nav className="bg-green-500 p-4 text-white">
+      <h1>Ini Menu Safari Buatan Rafa</h1>
+    </nav>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Menu from "./components/rafasya/Menu.jsx";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+
+      <Menu />
+
+      <h1 className="text-center mt-10">Selamat Datang di Basdead</h1>
+
+    </main>
+  );
+}
